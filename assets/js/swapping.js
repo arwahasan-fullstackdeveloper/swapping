980e80a736ee7effd4fd0ffc257a11bb2fe4e246
arwahasan-fullstackdeveloper <arwahasan374@gmail.com>
2024-08-21 05:59:56 -0700

    done

---
+++ assets/js/swapping.js
@@ -0,0 +1,9 @@
+let a =10;
+let b =20;
+document.write("<h1>before swapping the variable a contain "+a+"</h1>");
+document.write("<h1>before swapping the variable a contain "+b+"</h1>");
+a = a+b;
+b = a-b; 
+a =a-b;
+document.write("<h1>after swapping the variable a contain "+a+"</h1>");
+document.write("<h1>after swapping the variable a contain "+b+"</h1>");
